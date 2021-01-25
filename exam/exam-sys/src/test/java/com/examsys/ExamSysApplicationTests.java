1c976647cf08007bbb33ffbb513935a17b8570f3
48324134@qq.com <48324134@qq.com>
2021-01-25 09:53:29 +0800

    新项目

---
+++ exam/exam-sys/src/test/java/com/examsys/ExamSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ exam/exam-web/src/test/java/com/examweb/ExamWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
